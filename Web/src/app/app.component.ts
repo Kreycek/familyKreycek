d7c41c1407edf7d8e811a3bbc27f2f433e8cb73b
Ricardo Silva Ferreira <ricardo.ferreira.ext@spms.min-saude.pt>
2025-04-01 15:40:41 +0100

    ajustes

--- Web/src/app/app.component.ts
+++ Web/src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { FooterComponent } from "./footer/footer.component";
+import { MenuComponent } from "./menu/menu.component";
+import { CenterComponent } from "./center/center.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [MenuComponent, CenterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- Web/src/app/app.routes.ts
+++ Web/src/app/app.routes.ts
@@ -1,3 +1,111 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
 
-export const routes: Routes = [];
+
+export const routes: Routes = [
+
+    // { path: '', redirectTo: '/aplicacao', pathMatch: 'full' },   // Redireciona para login por padrão    
+    // { 
+    //     path:'aplicacao',
+    //     component:AplicacaoComponent,
+    //     children:[
+    //         {
+    //             path:'center',
+    //             component:CenterComponent,
+    //         },
+    //         {
+    //             path:'usuario',
+    //             component:UsuarioComponent,
+    //         }, 
+    //         {
+    //             path:'addUser/:id',
+    //             component:AddUsuarioComponent
+    //         }   , 
+    //         {
+    //             path:'addUser',
+    //             component:AddUsuarioComponent
+    //         },
+    //         {
+    //             path:'chartOfAccount',
+    //             component:ChartOfAccountsComponent
+    //         },
+    //         {
+    //             path:'addChartOfAccount',
+    //             component:AddChartOfAccountsComponent
+    //         }, 
+    //         {
+    //             path:'addChartOfAccount/:id',
+    //             component:AddChartOfAccountsComponent
+    //         }  , 
+    //         {
+    //             path:'daily/:id',
+    //             component:DailyComponent
+    //         } , 
+    //         {
+    //             path:'daily',
+    //             component:DailyComponent
+    //         }, 
+    //         {
+    //             path:'addDaily/:id',
+    //             component:AddDailyComponent
+    //         } , 
+    //         {
+    //             path:'addDaily',
+    //             component:AddDailyComponent
+    //         }, 
+    //         {
+    //             path:'company',
+    //             component:CompanyComponent
+    //         }, 
+    //         {
+    //             path:'addCompany/:id',
+    //             component:AddCompanyComponent
+    //         } , 
+    //         {
+    //             path:'addCompany',
+    //             component:AddCompanyComponent
+    //         }, 
+    //         {
+    //             path:'movement',
+    //             component:MovimentComponent
+    //         }, 
+    //         {
+    //             path:'addMovement/:id',
+    //             component:AddMovimentComponent
+    //         } , 
+    //         {
+    //             path:'addMovement',
+    //             component:AddMovimentComponent
+    //         }, 
+    //         {
+    //             path:'costCenter',
+    //             component:CostCenterComponent
+    //         }, 
+    //         {
+    //             path:'addCostCenter/:id',
+    //             component:AddCostCenterComponent
+    //         } , 
+    //         {
+    //             path:'addCostCenter',
+    //             component:AddCostCenterComponent
+    //         }, 
+    //         {
+    //             path:'balancete',
+    //             component:BalanceteComponent
+    //         }
+
+            
+    //     ]
+    // },   // Redireciona para login por padrão    
+    // { 
+    //     path:'login',
+    //     component:LoginComponent    
+    // }
+   
+];
+
+@NgModule({
+    imports: [],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
